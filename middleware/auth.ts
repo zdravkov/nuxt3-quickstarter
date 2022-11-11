225f9ebe7c6c73c065790c651813d4acf9324092
zdravkov <plamen.zdravkov@progress.com>
2022-11-11 22:42:23 +0200

    chore: remove log

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,5 +1,4 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-    console.log(111, to)
     if (to.params.id === 'test') {
      // return abortNavigation()
       return navigateTo('/')
